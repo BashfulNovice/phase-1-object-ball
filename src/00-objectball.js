eacdd56d9b9c8180a22a1906101b2ce254c4d9fc
Todd Krieger <twkriege@mtu.edu>
2021-09-23 09:59:57 -0400

    Build out stats structure

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,14 +1,67 @@
 function gameObject() {
     const gameInfo = {
         home: {
-            teamName: Brooklyn Nets
-            colors: ["Black", "White"]
-            players: ("Alan Anderson", "Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry")
+            teamName: "Brooklyn Nets",
+            colors: ["Black", "White"],
+            players: {"Alan Anderson": {
+                        number: "",
+                        shoe: "",
+                        points: "",
+                        rebounds: "",
+                        assists:"",
+                        steals: "",
+                        blocks: "",
+                        slamDunks: "",
+                    } 
+                    "Reggie Evans": {
+                        number: "",
+                        shoe: "",
+                        points: "",
+                        rebounds: "",
+                        assists:"",
+                        steals: "",
+                        blocks: "",
+                        slamDunks: "",
+                    }
+                    "Brook Lopez": {
+                        number: "",
+                        shoe: "",
+                        points: "",
+                        rebounds: "",
+                        assists:"",
+                        steals: "",
+                        blocks: "",
+                        slamDunks: "",
+                    }
+                    "Mason Plumlee": {
+                        number: "",
+                        shoe: "",
+                        points: "",
+                        rebounds: "",
+                        assists:"",
+                        steals: "",
+                        blocks: "",
+                        slamDunks: "",
+                    }
+                    "Jason Terry": {
+                        number: "",
+                        shoe: "",
+                        points: "",
+                        rebounds: "",
+                        assists:"",
+                        steals: "",
+                        blocks: "",
+                        slamDunks: "",
+                    }
         },
         away: {
-            teamName:
-            colors:
-            players:
+            teamName: "Charlotte Hornets",
+            colors: ["Turquoise", "Purple"],
+            players:{"Jeff Adrien":
+                    "Bismak Biyombo":
+                    "DeSagna Diop":
+                    "Ben Gordon":
+                    "Brendan Haywood":}
         };
     };
 }
