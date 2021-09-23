1fea3c7a5ba0a50b5cfc96f815f4fd4c5deb8770
Rodrigo Alba <rodrigoqalba@gmail.com>
2021-09-23 09:44:41 -0400

    updated

---
+++ src/00-objectball.js
@@ -0,0 +1,14 @@
+function gameObject() {
+    const gameInfo = {
+        home: {
+            teamName: Brooklyn Nets
+            colors: ["Black", "White"]
+            players: ("Alan Anderson", "Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry")
+        },
+        away: {
+            teamName:
+            colors:
+            players:
+        };
+    };
+}
